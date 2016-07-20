fe1d32532e5f4f51eb7627e5c79eb8508944f844
Kener <kener.linfeng@gmail.com>
2016-07-20 15:43:17 +0800

    get MP update

--- getMP.js
+++ getMP.js
@@ -3,19 +3,41 @@ var now = new Date(format('YYYY-MM-DD', new Date()) + ' 15:00:00')
 console.log('抢 ' + targetDay + ' at ' + now)
 var promotionAreas = {};
 
+if (!$('#kener').lenght) {
+  $('.main-content').prepend('<h3 id="kener" style="text-align:center">Get Ready!</h3>');
+}
+
+var idx = 1;
+function getReady() {
+  var t = new Date();
+  var mes = format('YYYY-MM-DD hh:mm:ss  ', t) + (idx++%2 ? '/' : "\\");
+  if (now - t < 400) {
+    mes += ' Go fresh~'
+    goFresh();
+  }
+  else {
+    setTimeout(getReady, 200);
+  }
+  // console.log(mes);
+  $('#kener').html(mes);
+  // top.document.getElementById('top-nav').innerHTML = ;
+}
+
+
 var timeTicket;
 var hasGet = {
-  24295: false,
-  24296: false,
-  24297: false,
-  24298: false,
-  24299: false,
-  24301: false,
-  24593: false,
-  24594: false
+  24296: false, // soho
+  24297: false, // 研发园
+  24298: false, // 东湖湾
+  24299: false, // 南湖
+  24301: false, // 酒仙桥
+  24302: false, // 央美
+  24593: false, // 电子城下
+  24594: false  // 电子城上
 };
 function goFresh () {
-  $.get('http://waimaieapp.meituan.com/ad/v1/buy/getPromotionArea?_=' + (now - 0), function(res){console.log(res);return;
+  console.log('~Freshing~')
+  $.get('http://waimaieapp.meituan.com/ad/v1/buy/getPromotionArea?_=' + (now - 0), function(res){
     if (res.msg != 'Success' || !res.data || !res.data.promotionAreas.length) {
       goFresh();
       return;
@@ -26,17 +48,17 @@ function goFresh () {
       promotionAreas[pList[i].id] = pList[i].positionInfo;
     }
     !hasGet[24296] && getPArea(24296);  // soho
-    !hasGet[24295] && getPArea(24295);  // ？
-    !hasGet[24297] && getPArea(24297);  // ？
-    !hasGet[24298] && getPArea(24298);  // ？
+    !hasGet[24297] && getPArea(24297);  // 研发园
+    // !hasGet[24298] && getPArea(24298);  // 东湖湾
     !hasGet[24299] && getPArea(24299);  // 南湖
     // !hasGet[24301] && getPArea(24301);  // 酒仙桥
+    // !hasGet[24302] && getPArea(24302);  // 央美
     // !hasGet[24593] && getPArea(24593);  // 电子城下
     // !hasGet[24594] && getPArea(24594);  // 电子城上
   });
 }
 // timeTicket = setInterval(goFresh, 250);
-goFresh()
+getReady();
 
 //adSpreadArea:[[{"y":116504769,"x":39981020},{"y":116505944,"x":39981983},{"y":116505100,"x":39984245},{"y":116504769,"x":39984245},{"y":116504769,"x":39981020}]],
 
@@ -47,7 +69,7 @@ function getPArea(id) {
   }
   var tarPos = false;
   for (var i = 0; i < posList.length; i++) {
-    if (posList[i].ableBuy) {
+    if (posList[i].ableBuy && posList[i].position > 5) {// 明天只买10名开外
       console.log('【find】 ' + id + ' position:' + posList[i].position + ' price:' + posList[i].price);
       tarPos = posList[i];
       break;
@@ -73,8 +95,12 @@ function getPArea(id) {
       },
       success: function (res) { //如果执行成功，那么执行此方法
         console.log(res);
-        if (res.data.msg != "Success") {
+        if (res.msg != "Success" && res.code != 4002) { // 不成功并且不是已经购买过此广告区内的广告位再试一次
           hasGet[id] = false;
+          getPArea(id); // 下一个
+        }
+        else {
+          $('#kener').html($('#kener').html() + '<br/>get ' + id + ' position ' + tarPos.position + ' price ' + tarPos.price);
         }
       },
       error: function (err) { //如果执行不成功，那么执行此方法
@@ -82,6 +108,9 @@ function getPArea(id) {
         hasGet[id] = false;
       }
     });
+  }
+  else {
+    $('#kener').html($('#kener').html() + '<br/>地区 ' + id + ' 都抢没了！');
   }
 }
 
